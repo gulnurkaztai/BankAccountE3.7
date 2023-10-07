83d5c2398710841b2fff4a899e2bb61825e6ee5f
gulnurkaztai <kaztai.gulnur@gmail.com>
2023-10-07 15:34:57 -0400

    first commit

---
+++ BankAccount.java
@@ -0,0 +1,47 @@
+/**
+ * A bank account has a balance that can be changed by deposits and withdrawals
+ */
+
+ public class BankAccount{
+    private double balance;
+
+    /**
+     * Constructs a bank account with a zero balance.
+     */
+
+     public BankAccount(){
+        this.balance = 0;
+     }
+
+     /**
+      * Constructs a bank account with a given balance.
+      @param initialBalance the initial balance.
+      */
+
+      public BankAccount(double initialBalance){
+        this.balance = initialBalance;
+      }
+
+      /**
+       * Deposits money into the bank account.
+       * @param amount the amount to deposit.
+       */
+
+       public void deposit(double amount){
+        this.balance = this.balance + amount;
+       }
+
+       /**
+        * Withdraws money from the bank account.
+        @param amount the amount to withdraw.
+        */
+
+        public void withdraw(double amount){
+            this.balance = this.balance - amount;
+        }
+
+        public double getBalance(){
+            return this.balance;
+        }
+
+ }
---
+++ BankAccountTest.java
@@ -0,0 +1,11 @@
+public class BankAccountTest {
+    
+    public static void main(String[] args){
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.deposit(1000);
+        bankAccount.withdraw(500);
+        bankAccount.withdraw(400);
+        System.out.println("Expected balance amount is 100$");
+        System.out.println(bankAccount.getBalance() + "$");
+    }
+}
